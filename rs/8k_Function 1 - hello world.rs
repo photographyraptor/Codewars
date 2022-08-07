1bdf42ed166da78d57a15bc334fd2f7ad24b9a1c
david.diazc <david.diazc@col.vueling.com>
2022-08-07 12:15:06 +0200

    rs1

---
+++ rs/8k_Function 1 - hello world.rs
@@ -0,0 +1,6 @@
+// Write a function greet() that returns "hello world!"
+// Remember to use the correct return type 
+fn greet() -> &'static str {
+    let s: &str = "hello world!";
+    return s;
+}
