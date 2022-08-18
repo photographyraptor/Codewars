90308608b15921aefda9c27f3f6cccb21678e780
photographyraptor <zdovahzaan@gmail.com>
2022-08-18 13:39:41 +0200

    sum pos rs

---
+++ rs/8k_Even or odd.rs
@@ -0,0 +1,8 @@
+//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+fn even_or_odd(i: i32) -> &'static str {
+    if i % 2 == 0 {
+        return "Even";
+    }
+    return "Odd";
+}
