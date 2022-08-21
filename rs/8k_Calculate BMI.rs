c08eced68606a4cdb3a87ba8345230c3550b40f0
photographyraptor <zdovahzaan@gmail.com>
2022-08-21 21:25:23 +0200

    bmi

---
+++ rs/8k_Calculate BMI.rs
@@ -0,0 +1,25 @@
+/*
+Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
+*/
+
+fn bmi(weight: u32, height: f32) -> &'static str {
+    let bmi = weight as f32 / (height * height);
+    if bmi <= 18.5 {
+        return "Underweight";
+    }
+    if bmi <= 25.0 {
+        return "Normal";
+    }
+    if bmi <= 30.0 {
+        return "Overweight";
+    }
+    return "Obese";
+}
