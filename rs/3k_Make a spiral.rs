846935ebc0f33befefd33f8fc339ddcd24434a4c
photographyraptor <zdovahzaan@gmail.com>
2022-08-28 21:02:53 +0200

    spiral done

--- rs/3k_Make a spiral.rs
+++ rs/3k_Make a spiral.rs
@@ -22,46 +22,43 @@ For example, spiral with size 5 should look like this:
 
 */
 
-//TODO:
-
 fn spiralize(size: usize) -> Vec<Vec<i8>> {
     let mut v = matrix(size);
     let mut w: Vec<Vec<Vec<usize>>> = Vec::new();
     w.push(vec![vec![0, 0]]);
     let mut vsize = size;
+    let mut dir = '>';
     
     for line in 0..size {
-        println!("");
-        print!("{}%4={} - ", line+1, (line+1) % 4);
-        print!("{}%3={} - ", line+1, (line+1) % 3);
-        print!("{}%2={} - ", line+1, (line+1) % 2);
-        print!("{}%1={}", line+1, (line+1) % 1);
-        if (line+1) % 4 == 0 {
-            vsize = vsize -2;
-            w.push(arriba(vsize, w.last().unwrap().last().unwrap().to_vec()));
-        } else if (line+1) % 3 == 0 {
-            w.push(izquierda(vsize, w.last().unwrap().last().unwrap().to_vec()));
-        } else if (line+1) % 2 == 0 {
+        
+        if dir == '>' {            
+            w.push(derecha(vsize, w.last().unwrap().last().unwrap().to_vec()));
+            dir = 'v';
+        }
+        else if dir == 'v' {
             if line >= 3 {
                 vsize = vsize -2;
             }
             w.push(abajo(vsize, w.last().unwrap().last().unwrap().to_vec()));
-        } else {        
-            w.push(derecha(vsize, w.last().unwrap().last().unwrap().to_vec()));
+            dir = '<';
+        } else if dir == '<' {
+            w.push(izquierda(vsize, w.last().unwrap().last().unwrap().to_vec()));
+            dir = '^';
+        } else {
+            vsize = vsize -2;
+            w.push(arriba(vsize, w.last().unwrap().last().unwrap().to_vec()));
+            dir = '>';        
         }
     }
     
     for w1 in w {
         for w2 in w1 {
-            //println!("{},{}", w2[0], w2[1]);
             v[w2[0]][w2[1]] = 1;
         }
     }
         
     return v;
 }
-
-
 
 fn derecha(vsize: usize, lastpos: Vec<usize>) -> Vec<Vec<usize>> {    
     let mut v: Vec<Vec<usize>> = Vec::new();
@@ -116,8 +113,6 @@ fn arriba(vsize: usize, lastpos: Vec<usize>) -> Vec<Vec<usize>> {
     
     return v;
 }
-
-
 
 fn matrix(size: usize) -> Vec<Vec<i8>> {
     let mut v: Vec<Vec<i8>> = Vec::new();
