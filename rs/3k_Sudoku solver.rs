9d8b095e27cfd436e342293d295bc85df70790f5
photographyraptor <zdovahzaan@gmail.com>
2022-08-29 19:40:21 +0200

    sudoku solved

--- rs/3k_Sudoku solver.rs
+++ rs/3k_Sudoku solver.rs
@@ -11,51 +11,47 @@ fn sudoku(puzzle: &mut [[u8; 9]; 9]) {
 fn sudoku_rec(puzzle: &mut [[u8; 9]; 9]) -> bool {
     let mut pos: Vec<u8>;
     let mut pos_h: Vec<u8>;
-    let mut pos_v: Vec<u8>;
-    let mut pos_s: Vec<u8>;
+    let pos_v: Vec<u8>;
+    let pos_s: Vec<u8>;
     
     for i in 0..9 {
-        for j in 0..9 {
+        pos_h = hor(*puzzle, i);
+        for j in 0..9 {            
             if puzzle[i as usize][j as usize] == 0 {
-                println!("FINDING: {},{}: ", i, j);
                 pos = Vec::new();
-                pos_h = hor(*puzzle, [i, j]);
-                pos_v = ver(*puzzle, [i, j]);
+                pos_v = ver(*puzzle, j);
                 pos_s = sqr(*puzzle, [i, j]);
                 
-                for o in pos_h {
+                for o in &pos_h {
                     if pos_v.contains(&o) && pos_s.contains(&o) {
-                        pos.push(o);
-                        println!("   p: {}", o);
+                        pos.push(*o);
                     }
                 }
                 
                 if pos.is_empty() {
-                    println!("EMPTY: {},{}   ", i, j);
+                    puzzle[i as usize][j as usize] = 0;
                     return false;
                 }
                 
                 for p in pos {
                     puzzle[i as usize][j as usize] = p;
-                    println!("TRY: {},{}->{}", i, j, p);
-                    if sudoku_rec(puzzle) {                        
-                        println!("TRUE");
+                    if sudoku_rec(puzzle) {
                         return true;
                     }
-                    println!("FAIL: {},{}->{}", i, j, p);
+                    puzzle[i as usize][j as usize] = 0;
                 }
+                return false;
             }
         }
     }
     return true;
 }
 
-
-fn hor(puzzle: [[u8; 9]; 9], pos: [u8; 2]) -> Vec<u8> {
+fn hor(puzzle: [[u8; 9]; 9], pos: u8) -> Vec<u8> {
     let mut v = Vec::new();    
-    let line = puzzle[pos[0] as usize];
+    let line = puzzle[pos as usize];
     
-    for i in 0..9 {
+    for i in 1..10 {
         if !line.contains(&i) {
             v.push(i);
         }
@@ -64,64 +60,44 @@ fn hor(puzzle: [[u8; 9]; 9], pos: [u8; 2]) -> Vec<u8> {
     return v;
 }
 
-
-fn ver(puzzle: [[u8; 9]; 9], pos: [u8; 2]) -> Vec<u8> {
-    let mut v = Vec::new();    
-    let mut line = Vec::new();
+fn ver(puzzle: [[u8; 9]; 9], pos: u8) -> Vec<u8> {
+    let mut v: Vec<u8> = vec![1,2,3,4,5,6,7,8,9];     
     
     for i in 0..9 {
-        line.push(puzzle[i as usize][pos[1] as usize]);
-    }
-    
-    for i in 1..9 {   
-        if !line.contains(&i) {
-            v.push(i);
-        }
+        v.retain(|&x|x != puzzle[i as usize][pos as usize]);
     }
     
     return v;
 }
-
 
 fn sqr(puzzle: [[u8; 9]; 9], pos: [u8; 2]) -> Vec<u8> {
-    let mut v = Vec::new();
-    let mut val;
-    let mut sqr: Vec<u8> = Vec::new();
-    let mut hp: Vec<usize> = Vec::new();
-    let mut vp: Vec<usize> = Vec::new();
+    let mut v: Vec<u8> = vec![1,2,3,4,5,6,7,8,9];    
+    let hp: [usize;3];
+    let vp: [usize;3];
     
     if pos[0] <= 2 {
-        hp = [0, 1, 2].to_vec();
+        hp = [0, 1, 2];
     }
-    else if pos[0] >= 3 && pos[0] <= 5 {
-        hp = [3, 4, 5].to_vec();
+    else if pos[0] <= 5 {
+        hp = [3, 4, 5];
     }
-    else if pos[0] >= 6 && pos[0] <= 8 {
-        hp = [6, 7, 8].to_vec();
+    else {
+        hp = [6, 7, 8];
     }
     
     if pos[1] <= 2 {
-        vp = [0, 1, 2].to_vec();
+        vp = [0, 1, 2];
     }
-    else if pos[1] >= 3 && pos[1] <= 5 {
-        vp = [3, 4, 5].to_vec();
+    else if pos[1] <= 5 {
+        vp = [3, 4, 5];
     }
-    else if pos[1] >= 6 && pos[1] <= 8 {
-        vp = [6, 7, 8].to_vec();
+    else {
+        vp = [6, 7, 8];
     }
     
     for i in hp {
         for j in &vp {
-            val = puzzle[i as usize][*j as usize];
-            if val > 0 {
-                sqr.push(val);   
-            }
-        }
-    }
-    
-    for i in 1..9 {
-        if !sqr.contains(&i) {
-            v.push(i);
+            v.retain(|&x|x != puzzle[i][*j]);
         }
     }
     
