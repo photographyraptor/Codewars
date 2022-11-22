5b7615b12eb96342d57761227e4af3c9ac00bfac
photographyraptor <zdovahzaan@gmail.com>
2022-11-22 12:50:33 +0100

    grass

---
+++ cpp/8k_Grasshopper - Terminal game combat function.cpp
@@ -0,0 +1,7 @@
+/*
+Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+*/
+
+int combat(int health, int damage){
+  return health - damage <= 0 ? 0 : health - damage;
+}
