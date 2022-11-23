20bd8998433c72576c56e60ab6c9a2b19a493fff
photographyraptor <zdovahzaan@gmail.com>
2022-11-23 21:13:09 +0100

    between

---
+++ cpp/8k_What is between.cpp
@@ -0,0 +1,15 @@
+/*
+Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+*/
+
+#include <vector>
+
+std::vector<int> between(int start, int end) {
+  std::vector<int> arr;
+  
+  for (int i = start; i <= end; i++) {
+      arr.push_back(i);
+  }
+  
+  return arr;
+}  
