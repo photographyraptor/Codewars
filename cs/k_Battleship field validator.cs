19cb4380bdaac84601a86949a455efa7c9dcc116
photographyraptor <zdovahzaan@gmail.com>
2022-12-15 13:41:28 +0100

    battleship balid

--- cs/k_Battleship field validator.cs
+++ cs/k_Battleship field validator.cs
@@ -15,56 +15,187 @@ This is all you need to solve this kata. If you're interested in more informatio
 namespace Solution {
   using System;
   using System.Collections.Generic;
+  using System.Linq;
   
   public class BattleshipField {
+    
     public static bool ValidateBattlefield(int[,] field) {
       var hs = GetHorizontals(field);
       var vs = GetVerticals(field);
+      var subs = GetSubmarines(hs, vs);
+      //PrintShips(hs);
+      //PrintShips(vs);
       
-      if (!ValidateIfTouching(hs, vs)) return false;
-      if (!ValidateIfMissing(hs, vs)) return false;
-      if (!ValidateIfMore(hs, vs)) return false;
+      if (!ValidateIfAllShips(hs, vs, subs)) return false;
+      if (!ValidateIfTouching(hs, vs, subs)) return false;
       
       return true;
     }
     
-    private static List<List<int>> GetHorizontals(int[,] field) {
+    private static List<List<List<(int, int)>>> GetHorizontals(int[,] field) {
+      var hs = new List<List<List<(int, int)>>>();
       
       for(var i=0; i<10; i++) {
+        var hsLine = new List<List<(int, int)>>();
+        var ship = new List<(int, int)>();
         for(var j=0; j<10; j++) {
-          if(field[i,j] == 1) {
-            
+          if (field[i,j] == 1) {
+            if (ship.Any() && ship.LastOrDefault().Item2 + 1 == j) {              
+              ship.Add((i,j));
+            }
+            else {
+              if (ship.Any()) {
+                hsLine.Add(ship);
+              }
+              ship = new List<(int, int)>();
+              ship.Add((i,j));
+            }
           }
         }
+        
+        if (ship.Any()) {
+              hsLine.Add(ship);
+            }
+        
+        hs.Add(hsLine);
       }
       
-      
-      return new List<List<int>>();
+      return hs;
     }
     
-    
-    private static List<List<int>> GetVerticals(int[,] field) {
+    private static List<List<List<(int, int)>>> GetVerticals(int[,] field) {
+      var vs = new List<List<List<(int, int)>>>();
       
       for(var j=0; j<10; j++) {
+        var vsLine = new List<List<(int, int)>>();
+        var ship = new List<(int, int)>();
         for(var i=0; i<10; i++) {
-          if(field[i,j] == 1) {
-            
+          if (field[i,j] == 1) {
+            if (ship.Any() && ship.LastOrDefault().Item1 + 1 == i) {              
+              ship.Add((i,j));
+            }
+            else {
+              if (ship.Any()) {
+                vsLine.Add(ship);
+              }
+              ship = new List<(int, int)>();
+              ship.Add((i,j));
+            }
           }
         }
+        
+        if (ship.Any()) {
+          vsLine.Add(ship);
+        }
+        
+        vs.Add(vsLine);
       }
-      return new List<List<int>>();
+      
+      return vs;
     }
     
-    private static bool ValidateIfTouching(List<List<int>> hs, List<List<int>> vs) {
-      return false;
+    private static bool ValidateIfAllShips(
+      List<List<List<(int, int)>>> hs, List<List<List<(int, int)>>> vs, List<(int, int)> subs) {
+      
+      var hBattleships = CountShipsOfXLenght(hs, 4);
+      var vBattleships = CountShipsOfXLenght(vs, 4);
+      if ((hBattleships + vBattleships) != 1) return false;
+      
+      var hCruisers = CountShipsOfXLenght(hs, 3);
+      var vCruisers = CountShipsOfXLenght(vs, 3);
+      if ((hCruisers + vCruisers) != 2) return false;
+      
+      var hDestros = CountShipsOfXLenght(hs, 2);
+      var vDestros = CountShipsOfXLenght(vs, 2);
+      if ((hDestros + vDestros) != 3) return false;
+      
+      if (subs.Count() != 4) return false;
+      
+      return true;
     }
     
-    private static bool ValidateIfMissing(List<List<int>> hs, List<List<int>> vs) {
-      return false;
+    private static bool ValidateIfTouching(
+      List<List<List<(int, int)>>> hs, List<List<List<(int, int)>>> vs, List<(int, int)> subs) {
+        
+      var hPosWithoutSub = 
+          from hLine in hs 
+          from ship in hLine where ship.Count() > 1 
+          from pos in ship
+          select pos;
+
+      var vPosWithoutSub = 
+          from vLine in vs 
+          from ship in vLine where ship.Count() > 1 
+          from pos in ship
+          select pos;
+      
+      foreach(var hp in hPosWithoutSub) {        
+        if (vPosWithoutSub.Any(v => (
+          Math.Abs(v.Item1 - hp.Item1) < 2)
+          && (Math.Abs(v.Item2 - hp.Item2) < 2))) return false;
+      }
+      
+      foreach(var vp in vPosWithoutSub) {
+        if (hPosWithoutSub.Any(h => (
+          Math.Abs(h.Item1 - vp.Item1) < 2)
+          && (Math.Abs(h.Item2 - vp.Item2) < 2))) return false;
+      }
+      
+      foreach(var s in subs) {
+        if (vPosWithoutSub.Any(v => (
+          Math.Abs(v.Item1 - s.Item1) < 2)
+          && (Math.Abs(v.Item2 - s.Item2) < 2)
+          && (Math.Abs(v.Item1 - s.Item1) > 0)
+          && (Math.Abs(v.Item2 - s.Item2) > 0))) return false;        
+        
+        if (hPosWithoutSub.Any(h => (
+          Math.Abs(h.Item1 - s.Item1) < 2)
+          && (Math.Abs(h.Item2 - s.Item2) < 2)
+          && (Math.Abs(h.Item1 - s.Item1) > 0)
+          && (Math.Abs(h.Item2 - s.Item2) > 0))) return false;
+      }
+      
+      return true;
     }
     
-    private static bool ValidateIfMore(List<List<int>> hs, List<List<int>> vs) {
-      return false;
+    
+    private static void PrintShips(List<List<List<(int, int)>>> ships){
+      foreach(var line in ships) {
+        foreach(var ship in line) {
+          foreach(var pos in ship) {
+            Console.WriteLine(pos.Item1.ToString() + "-" + pos.Item2.ToString());
+          }
+          Console.WriteLine(",");
+        }
+        Console.WriteLine("----");
+      }
+    }
+    
+    private static int CountShipsOfXLenght(List<List<List<(int, int)>>> ships, int l) {
+      return ships.Sum(s => s.Any(w2 => w2.Count() == l) ? 1 : 0);
+    }
+    
+    private static List<(int, int)> GetSubmarines(List<List<List<(int, int)>>> hs, List<List<List<(int, int)>>> vs) {
+
+      var hPos = (
+          from hLine in hs 
+          from ship in hLine 
+          where ship.Count() == 1 
+          select ship.First()).ToList();
+
+      var vPos = (
+          from vLine in vs 
+          from ship in vLine 
+          where ship.Count() == 1 
+          select ship.First()).ToList();
+
+      var subs =
+        from h in hPos
+        join v in vPos
+        on new { h.Item1, h.Item2 } equals new { v.Item1, v.Item2 }
+        select h;
+      
+      return subs.ToList();
     }
   }
 }
